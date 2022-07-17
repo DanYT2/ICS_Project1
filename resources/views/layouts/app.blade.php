ce638f581e26f228444b15441730475af3c3db37
Dan Wanjohi <danwwaititu@gmail.com>
2022-07-17 11:27:35 +0300

    Frontend design 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+      @extends('admin.layouts.header')
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -33,7 +34,7 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                @yield('content')
             </main>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
   Route::get('/dashboards', function (){
     return view('dashboards');
