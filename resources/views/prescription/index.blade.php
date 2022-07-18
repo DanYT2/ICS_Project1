f41515ac16025417807f1222f23e8bdd47914026
Dan Wanjohi <danwwaititu@gmail.com>
2022-07-18 23:24:10 +0300

    View prescription button for unprescribed patients.
    Video 75

--- resources/views/prescription/index.blade.php
+++ resources/views/prescription/index.blade.php
@@ -43,78 +43,28 @@
                     @endif
                   </td>
                      <td>
-                       <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="exampleModal">
+                       @if(!App\Models\Prescription::where('date', date('Y-m-d'))->where('doctor_id', auth()->user()->id)->where('user_id', $booking->user->id)->exists())
+                       <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal{{ $booking->user_id }}" >
                          Write prescription
                        </button>
+                       @include('prescription.form')
+                       @else
+                       View
+                       @endif
                      </td>
                 </tr>
 
               @empty
                 <td class="col-span-5">No appointments today</td>
               @endforelse
-
               </tbody>
             </table>
-
           </div>
-
         </div>
       </div>
-
     </div>
-    @if(count($bookings) > 0)
-      {{--MODAL--}}
-    {{--TODO: Modal not functional when button is pressed--}}
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-          <form action="{{ route('prescription') }}" method="post">
-            @csrf
-
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Prescription</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <input type="hidden" name="user_id" value="{{ $booking->user_id }}">
-                <input type="hidden" name="doctor_id" value="{{ $booking->doctor->id }}">
-                <input type="hidden" name="date" value="{{ $booking->date }}">
-
-                <div class="form-group">
-                  <label for="">Diagnosis</label>
-                  <input type="text" name="name_of_disease" class="form-control" required>
-                </div>
-                <div class="form-group">
-                  <label for="">Symptoms</label>
-                  <textarea name="symptoms" id="symptoms" cols="30" rows="10" class="form-control" placeholder="List of symptoms"></textarea>
-                </div>
-                <div class="form-group">
-                  <label for="">Prescription</label>
-                  <textarea name="medicine[]" id="medicine[]" cols="30" rows="10" class="form-control" placeholder="Prescription"></textarea>
-                </div>
-                <div class="form-group">
-                  <label for="">Medicine dosages</label>
-                  <textarea name="procedure_to_use_medicine" id="procedure_to_use_medicine" cols="30" rows="10" class="form-control" placeholder="Medicine Dosages"></textarea>
-                </div>
-                <div class="form-group">
-                  <label for="">Feedback</label>
-                  <textarea name="feedback" id="feedback" cols="30" rows="10" class="form-control" placeholder="Feedback"></textarea>
-                </div>
-                <div class="form-group">
-                  <label for="">Signature</label>
-                  <input type="text" name="signature" class="form-control" required>
-                </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Save changes</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    @endif
   </div>
+
 
 
 @endsection
